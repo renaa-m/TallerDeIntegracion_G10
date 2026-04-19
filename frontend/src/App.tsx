b680c5ab98e8a16bc937339160543e051c89eadc
Renata Manriquez <rmanriquezc02@gmail.com>
2026-04-19 01:21:45 -0400

    creacion login basico con auth0 y correccion de packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import LoginButton from './login.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +28,7 @@ function App() {
         >
           Count is {count}
         </button>
+        <LoginButton />
       </section>
 
       <div className="ticks"></div>
